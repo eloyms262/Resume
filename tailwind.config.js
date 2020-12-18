2ea7d7a495df8494d5d0448d96d31d687269bdc3
eloyms262 <eloyMs26@gmail.com>
2020-12-18 10:28:12 +0800

    2nd commit testing

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+purge: ["./*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        resume:"#182848",
+        resume2:"4B6CB7" 
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
